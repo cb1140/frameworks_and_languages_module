86e0366ae72d0c97084c21b4c28588f0dbfe14a3
Charlotte <cb1140@canterbury.ac.uk>
2021-12-13 16:59:05 +0000

    fixed the server test issue, changed api to app

--- server/sample.py
+++ server/sample.py
@@ -2,28 +2,14 @@ import falcon
 import json
 
 from wsgiref import simple_server
-from dataStore import ITEMS
+from datastore import ITEMS
 
-class QuoteResource:
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        quote = {
-            'quote': (
-                "I've always been more interested in "
-                "the future than in the past."
-            ),
-            'author': 'Grace Hopper'
-        }
-
-        resp.media = quote
-api.add_route('/quote', QuoteResource())
-api = falcon.API()
-
-"""Enable CORS policy for example.com and allows credentials"""
-app = falcon.App(middleware=falcon.CORSMiddleware(
-    allow_origins='example.com', allow_credentials='*'))
-
-
+class RootResource():
+   def on_get(self,req, resp):
+            """Handles main server request to pass test"""
+            resp.status= falcon.HTTP_200
+            resp.content_type = falcon.MEDIA_JSON
+            pass
 
 class GetResource:
     def on_get(self, req, resp):
@@ -33,23 +19,10 @@ class GetResource:
             resp.status = falcon.HTTP_200
             resp.content_type = falcon.MEDIA_JSON
 
-            fields = set(("user_id","keywords","description", "lat", "lon"))
-
-        """Returns right HTTP protocol if items cannot be found"""
-        if ITEMS.keys != fields:
-            resp.status = HTTP_204
-        else:
-            ITEMS[new_id] = req.json
-            ITEMS.add()
-            resp.status = falcon.HTTP_201
-
-
-
-
         
 class GetManyResource():
    def on_get(self,req, resp):
-       
+            """Handles GET MANY request """
             resp.status= falcon.HTTP_200
             resp.content_type = falcon.MEDIA_JSON
             pass
@@ -57,11 +30,18 @@ class GetManyResource():
 class PostResource:
     def on_post(self, req, resp):
         """Handles POST request """
-
-
         resp.media = {"id" : ITEMS.id, 'user_id' : ITEMS.user_id, 'description' : ""}
         resp.status = falcon.HTTP_201
         resp.content_type = falcon.MEDIA_JSON
+
+        fields = set(("user_id","keywords","description", "lat", "lon"))
+        """Returns right HTTP protocol if items cannot be found"""
+        if ITEMS.keys != fields:
+            resp.status = HTTP_204
+        else:
+            ITEMS[new_id] = req.json
+            ITEMS.add()
+            resp.status = falcon.HTTP_201
 
 
 class DeleteResource:
@@ -72,20 +52,22 @@ class DeleteResource:
         resp.content_type = falcon.MEDIA_JSON
 
 
-
+"""Enable CORS policy for example.com and allows credentials"""
+app = falcon.App(middleware=falcon.CORSMiddleware(
+    allow_origins='example.com', allow_credentials='*'))
 
 app = application = falcon.App()
 app.add_route('/item/{itemId}/', GetResource())
 app.add_route('/items/', GetManyResource())
 app.add_route('/item/', PostResource())
-api.add_route('/item/{itemId}/', DeleteResource())
-
+app.add_route('/item/{itemId}/', DeleteResource())
+app.add_route('/', RootResource())
 
 
 if __name__ == '__main__':
 
     from wsgiref import simple_server
-    httpd = simple_server.make_server("0.0.0.0", 8000, api)
+    httpd = simple_server.make_server("0.0.0.0", 8000, app)
     try:
         
         httpd.serve_forever()
